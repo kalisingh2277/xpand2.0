2dd46adedd0b77d06aaf56c8035faa94191b81a1
Harshit Mehra <168372241+Harshitmehra-270709@users.noreply.github.com>
2024-11-05 13:28:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Example of adding a simple animation on button click
-document.querySelector('button').addEventListener('click', function() {
-    this.style.transform = 'scale(1.1)';
-    setTimeout(() => {
-        this.style.transform = 'scale(1)';
-    }, 200);
-});
