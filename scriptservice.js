28be088cc3b2bc25f39f2a8a232c12b5d8eeb547
Harshit Mehra <168372241+Harshitmehra-270709@users.noreply.github.com>
2024-11-05 13:29:12 +0530

    Delete scriptservice.js

--- scriptservice.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const contactButton = document.querySelector('button');
-
-    contactButton.addEventListener('click', function() {
-        alert('Thank you for your interest! We will get back to you shortly.');
-    });
-});
